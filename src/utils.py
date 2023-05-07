02608c5e284adac658384267616f57361b78ccac
xwanaf <66076868+xwanaf@users.noreply.github.com>
2023-05-07 16:30:56 +0800

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -115,6 +115,48 @@ def configure_logging(logger_name):
 
 
 
+class ConfigWrapper(object):
+    """
+    Wrapper dict class to avoid annoying key dict indexing like:
+    `config.sample_rate` instead of `config["sample_rate"]`.
+    """
+    def __init__(self, **kwargs):
+        for k, v in kwargs.items():
+            if type(v) == dict:
+                v = ConfigWrapper(**v)
+            self[k] = v
+      
+    def keys(self):
+        return self.__dict__.keys()
+
+    def items(self):
+        return self.__dict__.items()
+
+    def values(self):
+        return self.__dict__.values()
+
+    def to_dict_type(self):
+        return {
+            key: (value if not isinstance(value, ConfigWrapper) else value.to_dict_type())
+            for key, value in dict(**self).items()
+        }
+
+    def __len__(self):
+        return len(self.__dict__)
+
+    def __getitem__(self, key):
+        return getattr(self, key)
+
+    def __setitem__(self, key, value):
+        return setattr(self, key, value)
+
+    def __contains__(self, key):
+        return key in self.__dict__
+
+    def __repr__(self):
+        return self.__dict__.__repr__()
+    
+    
 def removeOutliers(ad_sc_1k,cell_type):
     cell_types = np.unique(ad_sc_1k.obs[cell_type])
     valid_cells = []
