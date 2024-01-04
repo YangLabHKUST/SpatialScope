562c37b6ac26ee0e16ef7d34a47e80a0ec118b07
xwanaf <66076868+xwanaf@users.noreply.github.com>
2024-01-04 20:47:05 +0800

    Update Decomposition.py

--- src/Decomposition.py
+++ src/Decomposition.py
@@ -86,7 +86,7 @@ class GeneExpDecomposition:
             self.loggings.error('Wrong checkpoints/scRef file, checkpoint cell_class_column: {}, scRef cell_class_column: {}'.format(states['cell_class_column'],self.config.data.cell_class_column))
             sys.exit()
         
-        model_single = SCGradNN(input_dim1 = self.data.shape[1], down_block_dim=states['down_block_dim']).to(self.config.device)
+        model_single = SCGradNN(input_dim1 = self.data.shape[1], down_block_dim=states['down_block_dim'], factors = states['factors']).to(self.config.device)
         model = nn.DataParallel(model_single, device_ids = list(range(torch.cuda.device_count())))
 
         ema_helper = EMAHelper()
